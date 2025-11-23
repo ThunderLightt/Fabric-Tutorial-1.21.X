56dd91879580c2ac8a71f3175bdd5526b662d305
ThunderLightt <timsebastiandoering@gmail.com>
2025-11-23 16:14:10 +0100

    New items

--- src/main/java/net/timse/tutorialmod/TutorialMod.java
+++ src/main/java/net/timse/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.timse.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.timse.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
